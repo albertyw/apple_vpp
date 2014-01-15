a109e6a6de60b137dc51a3ba89619ea1438bb01e
Leonid Dinershtein <leonid.dinershtein@flatstack.com>
2014-01-15 13:21:46 +0400

    Inherit AppleVPP::Error from custom error class

--- lib/apple_vpp/error.rb
+++ lib/apple_vpp/error.rb
@@ -1,12 +1,12 @@
 module AppleVPP
-  module Error
+  class Error < ::StandardError
 
-    codes = [ 9600, 9601, 9602, 9603, 9604, 9605, 9606, 9607, 9608, 9609, 
-              9610, 9611, 9612, 9613, 9614, 9615, 9616, 9617, 9618, 9619, 
+    codes = [ 9600, 9601, 9602, 9603, 9604, 9605, 9606, 9607, 9608, 9609,
+              9610, 9611, 9612, 9613, 9614, 9615, 9616, 9617, 9618, 9619,
               9620, 9621, 9622, 9623 ]
 
     codes.each do |code|
-      self.const_set "Code#{code}", ( Class.new StandardError )
+      self.const_set "Code#{code}", ( Class.new AppleVPP::Error )
     end
 
   end
