492adfd456ad28822fe1b016bbef1fa9311ae5ff
Taylor Boyko <taylorboyko@gmail.com>
2015-07-09 14:15:20 -0700

    add retry-after handling

--- lib/apple_vpp/request.rb
+++ lib/apple_vpp/request.rb
@@ -7,12 +7,22 @@ module AppleVPP
     def self.submit( url, s_token = nil, body = {} )
       body['sToken'] = s_token
       body.delete_if { |_k, v| v.nil? }
-      
-      resp = RestClient.post url, body, content_type: :json
+
+      begin
+
+        resp = RestClient.post url, body, content_type: :json
+
+      rescue RestClient::ExceptionWithResponse => e
+
+        unless e.response.code == 503 raise e
+        raise AppleVPP::Error::ServiceUnavailable.new(e.response.raw_headers['Retry-After'])
+
+      end
+
       json = JSON.parse(resp)
 
       if json['status'] == -1
-        raise (eval "AppleVPP::Error::Code#{json['errorNumber']}"), json['errorMessage'] 
+        raise (eval "AppleVPP::Error::Code#{json['errorNumber']}"), json['errorMessage']
       end
 
       json
