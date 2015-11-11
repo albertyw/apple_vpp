5e8a656b624f346b7250fe60e65b42a327ae23d2
Albert Wang <albert@cellabus.com>
2015-11-10 17:27:52 -0800

    Allow unknown error codes to raise errors correctly

--- spec/apple_vpp/error_spec.rb
+++ spec/apple_vpp/error_spec.rb
@@ -6,5 +6,9 @@ describe AppleVPP::Error do
       error = AppleVPP::Error.get_error 9600
       expect(error).to eq AppleVPP::Error::Code9600
     end
+    it 'will return a default class if the error cannot be found' do
+      error = AppleVPP::Error.get_error 'asdf'
+      expect(error.to_s).to eq 'AppleVPP::Error::Codeasdf'
+    end
   end
 end
