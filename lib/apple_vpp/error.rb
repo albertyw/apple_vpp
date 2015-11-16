125a91b7ddadff64913893509429f1b8f3dd41e0
Taylor Boyko <taylorboyko@gmail.com>
2015-11-15 17:48:53 -0800

    add new error code

--- lib/apple_vpp/error.rb
+++ lib/apple_vpp/error.rb
@@ -4,7 +4,7 @@ module AppleVPP
   class Error < ::StandardError
     codes = [9600, 9601, 9602, 9603, 9604, 9605, 9606, 9607, 9608, 9609,
              9610, 9611, 9612, 9613, 9614, 9615, 9616, 9617, 9618, 9619,
-             9620, 9621, 9622, 9623, 9625, 9626]
+             9620, 9621, 9622, 9623, 9625, 9626, 9630 ]
 
     def self.make_error(error_code)
       const_set "Code#{error_code}", (Class.new AppleVPP::Error)
